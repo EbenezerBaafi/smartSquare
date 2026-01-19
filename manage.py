e6a2b9f14b497e9ccae5014a26294d8083402040
Aled Ebenezer Baafi Junior <cedicray20@gmail.com>
2026-01-19 23:21:35 +0000

    Initialized the project

--- manage.py
+++ manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'smartsqaure_backend.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'smartsquare_backend.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
--- smartsquare_backend/asgi.py
+++ smartsquare_backend/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for smartsqaure_backend project.
+ASGI config for smartsquare_backend project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'smartsqaure_backend.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'smartsquare_backend.settings')
 
 application = get_asgi_application()
