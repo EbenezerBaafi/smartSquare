a0621189b9f6be495824d97b67c014df6d0ff727
Aled Ebenezer Baafi Junior <cedicray20@gmail.com>
2026-03-02 17:39:44 +0000

    Added urls  for the properties app

--- properties/urls.py
+++ properties/urls.py
@@ -1,9 +1,22 @@
 from django.urls import path
-from .views import *
+from .views import (
+    PropertyListCreateView,
+    PropertyDetailView,
+    MyPropertiesView,
+    UploadPropertyImageView,
+    DeletePropertyImageView,
+    SavePropertyView,
+    UnsavePropertyView,
+    SavedPropertiesView
+)
 
 urlpatterns = [
-    # Add your property endpoints here
-    # Example:
-    # path('', PropertyListView.as_view(), name='property-list'),
-    # path('<int:pk>/', PropertyDetailView.as_view(), name='property-detail'),
+    path('', PropertyListCreateView.as_view(), name='property_list_create'),
+    path('<uuid:pk>/', PropertyDetailView.as_view(), name='property_detail'),
+    path('my-properties/', MyPropertiesView.as_view(), name='my_properties'),
+    path('<uuid:property_id>/upload-image/', UploadPropertyImageView.as_view(), name='upload_image'),
+    path('image/<uuid:pk>/', DeletePropertyImageView.as_view(), name='delete_image'),
+    path('<uuid:property_id>/save/', SavePropertyView.as_view(), name='save_property'),
+    path('<uuid:property_id>/unsave/', UnsavePropertyView.as_view(), name='unsave_property'),
+    path('saved/', SavedPropertiesView.as_view(), name='saved_properties'),
 ]
