bc131c98224637cec48f512572617d7fec9367c3
Aled Ebenezer Baafi Junior <cedicray20@gmail.com>
2026-03-02 16:51:50 +0000

    Added admin file to notifications

--- notifications/admin.py
+++ notifications/admin.py
@@ -1,3 +1,46 @@
 from django.contrib import admin
+from django.utils.html import format_html
+from .models import Notification
 
-# Register your models here.
+@admin.register(Notification)
+class NotificationAdmin(admin.ModelAdmin):
+    list_display = ('user', 'notification_type', 'title', 'is_read', 'created_at', 'status_badge')
+    list_filter = ('notification_type', 'is_read', 'created_at')
+    search_fields = ('user__email', 'user__full_name', 'title', 'message')
+    readonly_fields = ('created_at', 'read_at', 'status_badge')
+    ordering = ('-created_at',)
+    
+    fieldsets = (
+        ('User', {
+            'fields': ('user',)
+        }),
+        ('Notification Details', {
+            'fields': ('notification_type', 'title', 'message', 'metadata')
+        }),
+        ('Status', {
+            'fields': ('is_read', 'read_at', 'status_badge')
+        }),
+        ('Timestamp', {
+            'fields': ('created_at',),
+            'classes': ('collapse',)
+        }),
+    )
+    
+    def status_badge(self, obj):
+        if obj.is_read:
+            return format_html('<span style="color: green; font-weight: bold;">✓ Read</span>')
+        return format_html('<span style="color: orange; font-weight: bold;">● Unread</span>')
+    status_badge.short_description = "Status"
+    
+    actions = ['mark_as_read', 'mark_as_unread']
+    
+    def mark_as_read(self, request, queryset):
+        from django.utils import timezone
+        updated = queryset.update(is_read=True, read_at=timezone.now())
+        self.message_user(request, f'{updated} notification(s) marked as read.')
+    mark_as_read.short_description = "Mark as read"
+    
+    def mark_as_unread(self, request, queryset):
+        updated = queryset.update(is_read=False, read_at=None)
+        self.message_user(request, f'{updated} notification(s) marked as unread.')
+    mark_as_unread.short_description = "Mark as unread"
